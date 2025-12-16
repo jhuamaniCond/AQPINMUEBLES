4874acb2a77739969d4967d9aea76415f9e81940
JulioChura <jchuraaca@unsa.edu.pe>
2025-12-16 10:07:10 -0500

    se corrige la responividad en el form de login

--- aloja-aqp-frontend/src/components/authorization/LoginModal.vue
+++ aloja-aqp-frontend/src/components/authorization/LoginModal.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="show" class="fixed inset-0 bg-black/40 flex items-center justify-center z-[9999] backdrop-blur-sm">
-    <div class="bg-white w-full max-w-md p-8 rounded-2xl shadow-xl relative">
+  <div v-if="show" class="fixed inset-0 bg-black/40 flex min-h-screen items-center justify-center z-[9999] backdrop-blur-sm">
+      <div class="bg-white dark:bg-gray-900 rounded-lg shadow-lg w-full max-w-md p-6 relative flex flex-col items-center justify-center mx-auto my-auto">
       <!-- Botón de cerrar -->
       <button @click="$emit('close')" class="absolute top-3 right-3 text-gray-500 hover:text-gray-700">✕</button>
 
